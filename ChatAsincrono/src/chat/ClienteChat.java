ed6d5c523bba92c4e81d8c65425f3355b62cf204
luciaa77 <166419823+luciaa77@users.noreply.github.com>
2026-03-15 13:36:03 +0100

    ClienteChat creado
    Lucia

--- ChatAsincrono/src/chat/ClienteChat.java
+++ ChatAsincrono/src/chat/ClienteChat.java
@@ -1,5 +1,31 @@
 package chat;
 
+/**
+ * Clase ClienteChat
+ * Se encarga de conectarse al servidor y arrancar el hilo de envío.
+ */
 public class ClienteChat {
 
+    public static void main(String[] args) {
+
+        String host = "localhost";
+        int puerto = 5000;
+
+        try {
+
+            System.out.println("Conectando al servidor...");
+
+            Socket socket = new Socket(host, puerto);
+
+            System.out.println("Conectado al servidor");
+
+            HiloEnviar hiloEnviar = new HiloEnviar(socket);
+            hiloEnviar.start();
+
+        } catch (IOException e) {
+
+            System.out.println("Error en la conexión");
+            e.printStackTrace();
+        }
+    }
 }
